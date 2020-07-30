669b8da816c618d392173efb565e0390629aa53a
Leandro Ferreira <leanfj@hotmail.com>
2020-07-30 20:37:55 -0300

    Use Morgan and static file

--- node-express/index.js
+++ node-express/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const morgan = require('morgan')
 const http = require('http')
 
 const hostName = "localhost"
@@ -8,9 +8,11 @@ const port = 3000
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log(req.headers);
+app.use(morgan('dev'))
 
+app.use(express.static(`${__dirname}/public`))
+
+app.use((req, res, next) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
   res.end('<html><body><h1>This a Express server</h1></body></html>')
